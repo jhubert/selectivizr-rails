ac617539fe3fe4569e7ce0baf6c6b7a5221cf4d1
Jeremy Baker <jhubert@gmail.com>
2013-07-23 23:25:23 -0700

    Add a precompile hook for Rails asset pipeline support. Closes #5. Version 1.1.0

--- lib/selectivizr/rails/engine.rb
+++ lib/selectivizr/rails/engine.rb
@@ -1,7 +1,11 @@
 module Selectivizr
   module Rails
     class Engine < ::Rails::Engine
-      # auto wire
+      if Rails.version >= '3.1'
+        initializer 'Selectivizr precompile hook', :group => :all do |app|
+          app.config.assets.precompile += ['modernizr.js']
+        end
+      end
     end
   end
 end
