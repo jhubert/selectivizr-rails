6f73e885b30ef21da50058ab21be4eeb8f793f11
Jeremy Baker <jhubert@gmail.com>
2013-07-23 23:31:06 -0700

    Remove the rails 3.1 version check.
    There is really no reason to use this gem unless
    you're already on 3.1, so don't bother checking.

--- lib/selectivizr/rails/engine.rb
+++ lib/selectivizr/rails/engine.rb
@@ -1,10 +1,8 @@
 module Selectivizr
   module Rails
     class Engine < ::Rails::Engine
-      if Rails.version >= '3.1'
-        initializer 'Selectivizr precompile hook', :group => :all do |app|
-          app.config.assets.precompile += ['modernizr.js']
-        end
+      initializer 'Selectivizr precompile hook', :group => :all do |app|
+        app.config.assets.precompile += ['modernizr.js']
       end
     end
   end
