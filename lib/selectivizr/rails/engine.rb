a859620196761d614d9ffd9701b185a0edb3fbdf
Jeremy Baker <jhubert@gmail.com>
2013-07-26 01:11:10 -0700

    Fix a huge, glaring error thanks to @graff

--- lib/selectivizr/rails/engine.rb
+++ lib/selectivizr/rails/engine.rb
@@ -2,7 +2,7 @@ module Selectivizr
   module Rails
     class Engine < ::Rails::Engine
       initializer 'Selectivizr precompile hook', :group => :all do |app|
-        app.config.assets.precompile += ['modernizr.js']
+        app.config.assets.precompile += ['selectivizr.js']
       end
     end
   end
